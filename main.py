b4b222ce56ba8e3c4f936685791cef4a5f617c76
dhananjaypai08 <dhananjay2002pai@gmail.com>
2022-03-06 22:47:40 +0530

    build

--- main.py
+++ main.py
@@ -13,28 +13,36 @@ app.secret_key = "Not Defined yet"
 
 # print statements for personal understandings
 
+# Login
 class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    
+# Add to Cart
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer, nullable=False)
+    variety = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
 @app.route("/")
 def start():
-    if 'loggedin' in session:
-        return redirect(url_for('home'))
     return redirect(url_for('login'))
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
-    print(session)
+    
     if 'loggedin' in session:
-        return render_template('index.html')
-    return redirect(url_for('login'))
+        return redirect(url_for('home'))
+    return render_template('loginpage.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    msg = ''
     if request.method == 'POST':
         entered_fname = request.form.get('firstname')
         entered_lname = request.form.get('lastname')
@@ -43,58 +51,65 @@ def register():
         entered_cpass = request.form.get('password-confirm')
         print(entered_fname, entered_lname, entered_email, entered_pass, entered_cpass)
         if not entered_fname or not entered_lname or not entered_email or not entered_pass or not entered_cpass:
-            print('empty field')
-            return redirect(url_for('register'))
+            msg = 'empty field'
+            return render_template('signup.html',msg=msg)
         if entered_pass != entered_cpass:
-            print('cemail!=email')
-            return redirect(url_for('register'))
+            msg = 'entered password and confirmed password does not match'
+            return render_template('signup.html',msg=msg)
         check = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         validity = re.fullmatch(check, entered_email)
         if not validity:
-            print('email not proper')
-            return redirect(url_for('register'))
+            msg = 'Please Enter a valid email address'
+            return render_template('signup.html',msg=msg)
         email_exists = Login.query.filter_by(email=entered_email).all()
         if email_exists:
-            print('email already exists')
-            return redirect(url_for('register'))
+            msg ='Email already exists'
+            return render_template('signup.html',msg=msg)
         new_user = Login(first_name=entered_fname, last_name=entered_lname, email=entered_email, password=entered_pass)
         db.session.add(new_user)
         db.session.commit()
-        print('end')
-        return redirect(url_for('login'))
-    return render_template('signup.html')
+        msg = 'registered successfully'
+    return render_template('signup.html',msg=msg)
         
         
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    msg = ''
     if request.method == 'POST':
         entered_email = request.form.get('email')
         entered_pass = request.form.get('password')
         print(entered_email, entered_pass)
         if not entered_email or not entered_pass:
-            print('no email or pass')
-            return redirect(url_for('login'))
+            msg = 'Please Enter your email and password'
+            return render_template('loginpage.html',msg=msg)
         user = Login.query.filter_by(email=entered_email).first()
         if not user:
-            print('no user with that email')
-            return redirect(url_for('register'))
+            msg = 'Email does not exist'
+            return render_template('signup.html',msg=msg)
         print(user.password, entered_pass)
-        if user.password==entered_pass:
-            print('right credentials')
-            session['loggedin'] = True
-            session['id'] = user.id
-            session['email'] = user.email
-            return redirect(url_for('home'))
+        if user.password!=entered_pass:
+            msg = 'Email or password incorrect'
+            return render_template('loginpage.html',msg=msg)
+        msg = 'Logged in Successfully'
+        session['loggedin'] = True
+        session['id'] = user.id
+        session['email'] = user.email
+        return render_template('index.html',msg=msg)
         
-    return render_template('loginpage.html')
+    return render_template('loginpage.html',msg=msg)
 
 @app.route("/logout")
 def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('email', None)
+    msg = "Logged out Successfully"
     print(session)
     return redirect(url_for('login'))
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 @app.route("/almond", methods=['GET', 'POST'])
 def almond():
@@ -103,6 +118,7 @@ def almond():
 @app.route("/kaju", methods=['GET', 'POST'])
 def kaju():
     return render_template('kaju.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)
